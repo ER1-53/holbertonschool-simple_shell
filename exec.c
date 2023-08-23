7bcb9b16d55455ea8221436aab4c377e273fb667
Kevin.P <kevin.pellissard@gmail.com>
2023-08-23 14:09:57 +0200

    Fix the check env, change main.h, change exit value in main.c

--- exec.c
+++ exec.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * parse_cmd - parse the string and execute the command
+ * execute_cmd - check the command and execut if it's success
  *
  * @name: name of command
  * @buffer: string pass in the buffer
@@ -9,7 +9,7 @@
  * @status: status of function
  */
 void parse_cmd(char *buffer, char *name, int nb_cmd, char **env,
-			int *status)
+				 int *status)
 {
 	int i = 0;
 	char *copy_cmd, **cmd, *token;
@@ -26,20 +26,44 @@ void parse_cmd(char *buffer, char *name, int nb_cmd, char **env,
 	cmd[i] = NULL;
 	copy_cmd = strdup(cmd[0]);
 
-	cmd[0] = _which(copy_cmd, env);
-	if (!cmd[0])
-		cmd_null(name, buffer, cmd, copy_cmd, nb_cmd, status);
-	else
+	if (check_env(copy_cmd, env) != 0)
 	{
-		exe_cmd(cmd, name, env);
-		wait(status);
+		cmd[0] = _which(copy_cmd, env);
+		if (!cmd[0])
+			cmd_null(name, buffer, cmd, copy_cmd, nb_cmd, status);
+		else
+		{
+			exe_cmd(cmd, name, env);
+			wait(status);
+		}
+		if (*status != 127)
+			*status /= 256;
+		free(cmd[0]);
 	}
-	if (*status != 127)
-		*status /= 256;
-	free(cmd[0]);
 	free(cmd);
 	free(token);
 	free(copy_cmd);
+}
+
+/**
+ * check_env - Check env command
+ *
+ * @copy_cmd: The command
+ * @env: The environment variables
+ * Return: 0 if success
+ */
+
+int check_env(char *copy_cmd, char **env)
+{
+	int i;
+	if (strcmp(copy_cmd, "env") == 0)
+	{
+		for (i = 0; env[i]; i++)
+			printf("%s\n", env[i]);
+		return (0);
+	}
+	else
+		return (1);
 }
 
 /**
@@ -50,12 +74,13 @@ void parse_cmd(char *buffer, char *name, int nb_cmd, char **env,
  */
 void exe_cmd(char **cmd, char *name, char **env)
 {
+
 	if (fork() == 0)
 	{
 		if (execve(cmd[0], cmd, env) == -1)
 		{
 			perror(name);
-			exit(-1);
+			exit(0);
 		}
 	}
 }
@@ -103,33 +128,4 @@ void cmd_null(char *name, char *str, char **cmd, char *copy_cmd, int nb_cmd,
 		printf("%s: %d: %s: not found\n", name, nb_cmd, copy_cmd);
 		*status = 127;
 	}
-}
-
-/**
- * exit_value - calculate the exit value
- * @n: supposed value of exit
- * Return: -1 for illegal numbers or a number between 0 and 255
- */
-int exit_value(char *n)
-{
-	unsigned int nb = 0;
-
-	if (!n)
-		return (-2);
-
-	for (; *n; n++)
-	{
-		if (*n < '0' || *n > '9')
-			return (-1);
-
-		nb = nb * 10 + (*n - '0');
-
-		if (nb > 2147483648)
-			return (-1);
-	}
-
-	while (nb > 255)
-		nb -= 256;
-
-	return (nb);
 }
--- main.h
+++ main.h
@@ -16,12 +16,13 @@ void interactive(char *name, char *buffer, size_t bufsize, int nb_cmd,
 void non_interactive(char *name, char *buffer, size_t bufsize, int nb_cmd,
 					 char **env, int *status);
 void parse_cmd(char *buffer, char *name, int nb_cmd, char **env,
-				 int *status);
+			   int *status);
 void exe_cmd(char **cmd, char *name, char **env);
 char *_which(char *cmd, char **env);
 char *_getenv(const char *name, char **env);
 void cmd_null(char *name, char *str, char **cmd, char *copy_cmd, int nb_cmd,
 			  int *status);
+int check_env(char *copy_cmd, char **env);
 int exit_value(char *n);
 
 #endif /* MAIN_H */
