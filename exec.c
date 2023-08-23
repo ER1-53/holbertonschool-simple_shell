6efa7794c1493fd0e81303484f815276e9e5037e
Kevin.P <kevin.pellissard@gmail.com>
2023-08-23 17:39:55 +0200

    Fix exit function

--- exec.c
+++ exec.c
@@ -127,32 +127,3 @@ void cmd_null(char *name, char *str, char **cmd, char *copy_cmd, int nb_cmd,
 		*status = 127;
 	}
 }
-
-/**
- * exit_value - calculate the exit value
- * @n: supposed value of exit
- * Return: -1 for illegal numbers or a number between 0 and 255
- */
-int exit_value(char *n)
-{
-	unsigned int nb = 0;
-
-	if (!n)
-		return (-2);
-
-	for (; *n; n++)
-	{
-		if (*n < '0' || *n > '9')
-			return (-1);
-
-		nb = nb * 10 + (*n - '0');
-
-		if (nb > 2147483648)
-			return (-1);
-	}
-
-	while (nb > 255)
-		nb -= 256;
-
-	return (nb);
-}
